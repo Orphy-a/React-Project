ed982a66a368d9caf41f6f6afe6ea028da6ab4e1
jaeyoung0560 <jaeyoung0560@gmail.com>
2025-06-21 12:34:25 +0900

    메인 css

--- shopping Mall/src/components/Header.jsx
+++ shopping Mall/src/components/Header.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import "../styles/header.css";
 
 const Header = () => {
   return (
     <>
-      <div className="header">
-        <div className="header-logo">
-          <a href="#">로고</a>
-        </div>
-        <div className="header-nav">
-          <li>
-            <a href="#">메뉴1</a>
-          </li>
-          <li>
-            <a href="#">메뉴2</a>
-          </li>
+      <div className="header-container">
+        <div className="header">
+          <div className="header-logo">
+            <a href="#">로고</a>
+          </div>
+          <div className="header-nav">
+            <li>
+              <a href="#">메뉴1</a>
+            </li>
+            <li>
+              <a href="#">메뉴2</a>
+            </li>
+          </div>
         </div>
       </div>
     </>
--- shopping Mall/src/components/Main.jsx
+++ shopping Mall/src/components/Main.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "../styles/main.css";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <>
+      <div className="main-container">
+        <div className="main-bg"></div>
+        <div className="main-container">
+          <div className="main-row">
+            <div className="main-col">
+              <img src="" />
+              <h4>상품명</h4>
+              <p>상품정보</p>
+            </div>
+            <div className="main-col">
+              <img src="" />
+              <h4>상품명</h4>
+              <p>상품정보</p>
+            </div>
+            <div className="main-col">
+              <img src="" />
+              <h4>상품명</h4>
+              <p>상품정보</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Main;
