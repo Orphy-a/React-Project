c517f7f300e3c80b6a572440ceebf74fbca6cf65
jaeyoung0560 <jaeyoung0560@gmail.com>
2025-07-21 14:20:50 +0900

    메인 css 수정 및 보완

--- shopping Mall/src/components/Main.jsx
+++ shopping Mall/src/components/Main.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import "../styles/main.css";
 
+const imageNames = ["shoes1.jpg", "shoes2.jpg", "shoes3.jpg"];
+
 const Main = () => {
   return (
     <>
       <div className="main-container">
         <div className="main-bg"></div>
-        <div className="main-container">
-          <div className="main-row">
-            <div className="main-col">
-              <img src="" />
-              <h4>상품명</h4>
-              <p>상품정보</p>
-            </div>
-            <div className="main-col">
-              <img src="" />
-              <h4>상품명</h4>
-              <p>상품정보</p>
-            </div>
-            <div className="main-col">
-              <img src="" />
-              <h4>상품명</h4>
-              <p>상품정보</p>
-            </div>
+        <div className="main-row">
+          <div className="main-product">
+            <img className="main-product-img" src="https://codingapple1.github.io/shop/shoes1.jpg" />
+            <div>상품명</div>
+            <div>상품가격</div>
+          </div>
+          <div className="main-product">
+            <img className="main-product-img" src="https://codingapple1.github.io/shop/shoes2.jpg" />
+            <div>상품명</div>
+            <div>상품가격</div>
+          </div>
+          <div className="main-product">
+            <img className="main-product-img" src="https://codingapple1.github.io/shop/shoes3.jpg" />
+            <div>상품명</div>
+            <div>상품가격</div>
           </div>
         </div>
       </div>
