1123a8fdd3bdfeb23e00a8e5a882a127023a9571
jaeyoung0560 <jaeyoung0560@gmail.com>
2025-07-23 18:00:24 +0900

    useEffect 연습

--- shopping Mall/src/components/Details.jsx
+++ shopping Mall/src/components/Details.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import data from "../data/data.jsx";
 import { useParams } from "react-router-dom";
 import "../styles/detail.css";
@@ -13,6 +13,17 @@ const Details = () => {
   // 데이터에서 숫자로 변환한 id로 물건 정보 찾기
   const product = data.find((item) => item.id === productId);
 
+  // 생명주기 mount - update - unmount
+  // mount, update시 코드 실행됨
+  // 어려운 연산, 서버에서 데이터 가져오는 작업, 타이머에 주로 사용
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+  }, []);
+
+  const [alert, setAlert] = useState(true);
+
   return (
     <>
       <div className="detail-product-container">
@@ -21,9 +32,11 @@ const Details = () => {
           <div>{product.title}</div>
           <div>{product.content}</div>
           <div>{product.price.toLocaleString()} 원</div>
+
           <button className="detail-product-order-btn">주문하기</button>
         </div>
       </div>
+      {alert == true ? <div className="alert">useEffect, setTimeout 연습</div> : null}
     </>
   );
 };
