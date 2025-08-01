7a1d96189ed3f97533cf51a309e92cea64e1a5b0
jaeyoung0560 <jaeyoung0560@gmail.com>
2025-08-01 16:39:01 +0900

    ajax 통신

--- shopping Mall/src/components/Main.jsx
+++ shopping Mall/src/components/Main.jsx
@@ -1,6 +1,7 @@
 import "../styles/main.css";
 import data from "../data/data.jsx";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Main = () => {
   const navigate = useNavigate();
@@ -11,13 +12,28 @@ const Main = () => {
         <div className="main-bg"></div>
         <div className="main-row">
           {data.map((shoes) => (
-            <div className="main-product" key={shoes.id} onClick={() => navigate(`/detail/${shoes.id}`)} style={{ cursor: "pointer" }}>
-              <img className="main-product-img" src={`https://codingapple1.github.io/shop/shoes${shoes.id}.jpg`} alt={shoes.title} />
+            <div className="main-product" key={shoes.id}>
+              <img className="main-product-img" onClick={() => navigate(`/detail/${shoes.id}`)} style={{ cursor: "pointer" }} src={`https://codingapple1.github.io/shop/shoes${shoes.id}.jpg`} alt={shoes.title} />
               <div>{shoes.title}</div>
               <div>{shoes.price}</div>
             </div>
           ))}
         </div>
+        <button
+          className="main-more-btn"
+          onClick={() => {
+            axios
+              .get(`http://localhost:5173/detail/${shoes.id}`)
+              .then((data) => {
+                console.log(data.data);
+              })
+              .catch(() => {
+                console.log("요청 실패");
+              });
+          }}
+        >
+          더보기
+        </button>
       </div>
     </>
   );
